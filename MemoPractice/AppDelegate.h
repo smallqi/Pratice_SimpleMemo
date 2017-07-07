df3b751bea320cc0e48ac1ab8116aa775c3e836d
littleqi <zlittleqi.com>
2017-07-07 20:29:29 +0800

    firsr creat Project

---
+++ MemoPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MemoPractice
+//
+//  Created by BlackApple on 2017/7/7.
+//  Copyright © 2017年 BlackApple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
